a6ca28a43253878bc16d37b112234a17a06123de
Alex <mendelberg44@gmail.com>
2021-02-24 12:06:17 +0200

    refactoring is done

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import tools
 
-amount = int(input('Amount structures: '))
-workers_data = []
-
-for x in range(amount):
-    print(f'The person №{x + 1}')
-    workers_data.append(tools.Firma(
+tools.put_data('data', [
+    tools.Firma(
         input('Name: '),
         input('Position: '),
         int(input('Year: '))
-    ))
-
-tools.put_data('data', workers_data)
+    )
+    for _ in range(
+        int(input('Amount structures: '))
+    )
+])
--- short_data.py
+++ short_data.py
@@ -1,7 +1,7 @@
 import tools
 
-workers_data = tools.get_arr_data('data')
-
-tools.sort_data(workers_data)
-
-tools.put_data('data_shorted', workers_data)
+tools.put_data('data_shorted',
+               tools.sort_data(
+                   tools.get_arr_data('data')
+               )
+               )
